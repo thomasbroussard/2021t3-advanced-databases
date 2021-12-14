e66a99b6f2afd5915c768c3e42e0898d27880677
thomas <thomas.broussard@local.local>
2021-12-14 14:57:38 +0100

    * using the node execution to run queries

--- dblp/search-queries.js
+++ dblp/search-queries.js
@@ -1,6 +1,21 @@
-let example = db.publications.aggregate(
-    {$match : {type:"Article"}},
-    {$project : {authors:1}}
-).toArray()
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27217/dblp';
 
-console.log(example)
+MongoClient.connect(url, function(err, dbClient) {
+
+    var db = dbClient.db("dblp")
+
+    db.collection("publications")
+        .findOne({}, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+
+    db.collection("publications")
+        .distinct("type", function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+});
+
