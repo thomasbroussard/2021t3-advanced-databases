4304f2d5bc72e485d6b4e0e701a6e64551e2847c
thomas <thomas.broussard@local.local>
2021-12-17 15:00:18 +0100

    * query through js

--- dblp/search-queries.js
+++ dblp/search-queries.js
@@ -1,22 +1,24 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27217/dblp';
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27217/dblp';
 
 MongoClient.connect(url, function(err, dbClient) {
 
-    var db = dbClient.db("dblp")
+    let db = dbClient.db("dblp")
 
-    db.collection("publications")
+    let publications = db.collection("publications");
+    
+    publications
         .findOne({}, function(err, result) {
         if (err) throw err;
         console.log(result);
     });
-
-
-    db.collection("publications")
-        .distinct("type", function(err, result) {
-            if (err) throw err;
-            console.log(result);
-        });
+    //
+    //
+    // publications
+    //     .distinct("type", function(err, result) {
+    //         if (err) throw err;
+    //         console.log(result);
+    //     });
 });
 
 
