a65337286d9660c286a881c6dc1d2a931a670ae8
thomas <thomas.broussard@local.local>
2021-12-17 16:36:35 +0100

    * query through js

--- companies/search-queries-from-scratch.js
+++ companies/search-queries-from-scratch.js
@@ -7,6 +7,36 @@ MongoClient.connect(url, function(err, dbClient) {
     let db = dbClient.db("samples")
     let companies = db.collection("companies");
 
-    companies.countDocuments((err, n)=> console.log(n) );
+    // companies.countDocuments((err, n)=> console.log(n) );
+    // companies.findOne({}, (err, res) => {
+    //     console.log(res);
+    // })
+
+    // js-01
+    // companies.findOne({'name': 'Facebook'}, (cmdErr, result) => {
+    //     console.log(result.name);
+    // });
+
+    // js-02
+    agg = [
+        {
+            '$match': {
+                'founded_year.$numberInt': '2005'
+            }
+        },
+        {
+            '$project': {
+                name:1,
+                'founded_year':1
+            }
+
+        },{
+            '$limit': 3
+        }
+    ];
+    let promise = companies.aggregate(agg).toArray();
+    promise.then((value => {
+        console.log(value);
+    }))
 
 });
